51e2e0eeeb16f81df5bf83e313f1d767e62771dc
BFiato <BradFiato@gmail.com>
2025-12-14 00:24:32 -0500

    F: added new func to main.py

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
         return file_contents
+
+def word_counter(file_contents):
+    return len(file_contents.split())
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    print( f"Found {word_counter(get_book_text("books/frankenstein.txt"))} total words")
     
 main()
