1982dd0ed2abd4359426e87f382565e2b9ac8fff
BFiato <BradFiato@gmail.com>
2025-12-16 01:23:52 -0500

    I: added sort func to stats.py updated main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_num_chars
+from stats import *
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -9,5 +9,14 @@ def get_book_text(path_to_file):
    # return len(file_contents.split())
 def main():
     #print( f"Found {get_num_words(get_book_text("books/frankenstein.txt"))} total words")
-    print(get_num_chars(get_book_text("books/frankenstein.txt")))
+    #print(get_num_chars(get_book_text("books/frankenstein.txt")))
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {get_num_words(get_book_text('books/frankenstein.txt'))} total words")
+    print("--------- Character Count -------")
+    for item in sort_chars(get_num_chars(get_book_text("books/frankenstein.txt"))):
+        if item["char"].isalpha():
+            print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -8,5 +8,13 @@ def get_num_chars(file_contents):
         if letter in num_chars:
             num_chars[letter] +=1
         else:
-            num_chars[letter] = 1
+            num_chars[letter] =1
     return num_chars
+def sort_on(items):
+    return items["num"]
+def sort_chars(num_chars):
+    dicts_list = []
+    for key, value in num_chars.items():
+        dicts_list.append({"char": key, "num": value}) 
+    dicts_list.sort(reverse=True, key=sort_on)
+    return dicts_list
