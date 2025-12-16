82e432b99864c0a434ed42fc8fe3e39507f7dc02
BFiato <BradFiato@gmail.com>
2025-12-16 01:40:40 -0500

    J: added sys.argument to main.py

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
+import sys
 from stats import *
+
+if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+else:
+    book_path = sys.argv[1]
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
         return file_contents
 
-# def word_counter(file_contents):
-   # return len(file_contents.split())
 def main():
-    #print( f"Found {get_num_words(get_book_text("books/frankenstein.txt"))} total words")
-    #print(get_num_chars(get_book_text("books/frankenstein.txt")))
+
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path} ...")
     print("----------- Word Count ----------")
-    print(f"Found {get_num_words(get_book_text('books/frankenstein.txt'))} total words")
+    print(f"Found {get_num_words(get_book_text(book_path))} total words")
     print("--------- Character Count -------")
-    for item in sort_chars(get_num_chars(get_book_text("books/frankenstein.txt"))):
+    for item in sort_chars(get_num_chars(get_book_text(book_path))):
         if item["char"].isalpha():
             print(f"{item['char']}: {item['num']}")
     print("============= END ===============")
